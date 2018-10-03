23a9fd77cf8b8463330581318f8536a050180dd6
Kristinn Jónsson <kristinn.thor.jonsson@viewcare.com>
2018-10-03 18:17:03 +0200

    Excercises 5.1 and 5.3

--- exercise05/TestCountPrimesTasks.java
+++ exercise05/TestCountPrimesTasks.java
@@ -15,204 +15,195 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.function.IntToDoubleFunction;
 
 
 public class TestCountPrimesTasks {
-  private static final ExecutorService executor 
-    = Executors.newWorkStealingPool();
-  //  = Executors.newCachedThreadPool();
-  
-  public static void main(String[] args) {
-    SystemInfo();
-    final int range = 100_000;
-    System.out.println(Mark7("countSequential", 
-			     i -> countSequential(range)));
-    System.out.println(Mark7(String.format("countParTask1 %6d", 32), 
-			     i -> countParallelN1(range, 32)));
-    System.out.println(Mark7(String.format("countParTask2 %6d", 32), 
-			     i -> countParallelN2(range, 32)));
-    System.out.println(Mark7(String.format("countParTask3 %6d", 32), 
-			     i -> countParallelN3(range, 32)));
-    for (int c=1; c<=100; c++) {
-      final int taskCount = c;
-      Mark7(String.format("countParTask1 %6d", taskCount), 
-	    i -> countParallelN1(range, taskCount));
-      
+    //private static final ExecutorService executor = Executors.newWorkStealingPool();
+    private static final ExecutorService executor = Executors.newCachedThreadPool();
+
+
+    public static void main(String[] args) {
+        SystemInfo();
+        final int range = 100_000;
+
+        for (int nrOfTasks = 1; nrOfTasks <= 12; nrOfTasks++) {
+            int currentNrOfTasks = nrOfTasks;
+            System.out.println(Mark7(String.format("countParTask1 %6d", nrOfTasks),
+                    i -> countParallelN1(range, currentNrOfTasks)));
+        }
     }
-    for (int c=1; c<=100; c++) {
-      final int taskCount = c;
-      Mark7(String.format("countParTask2 %6d", taskCount), 
-	    i -> countParallelN2(range, taskCount));
+
+    private static boolean isPrime(int n) {
+        int k = 2;
+        while (k * k <= n && n % k != 0)
+            k++;
+        return n >= 2 && k * k > n;
     }
-  }
 
-  private static boolean isPrime(int n) {
-    int k = 2;
-    while (k * k <= n && n % k != 0)
-      k++;
-    return n >= 2 && k * k > n;
-  }
-
-  // Sequential solution
-  private static long countSequential(int range) {
-    long count = 0;
-    final int from = 0, to = range;
-    for (int i=from; i<to; i++)
-      if (isPrime(i)) 
-        count++;
-    return count;
-  }
-
-  // General parallel solution, using multiple (Runnable) tasks
-  private static long countParallelN1(int range, int taskCount) {
-    final int perTask = range / taskCount;
-    final LongCounter lc = new LongCounter();
-    List<Future<?>> futures = new ArrayList<Future<?>>();
-    for (int t=0; t<taskCount; t++) {
-      final int from = perTask * t, 
-        to = (t+1 == taskCount) ? range : perTask * (t+1); 
-      futures.add(executor.submit(() -> { 
-          for (int i=from; i<to; i++)
+    // Sequential solution
+    private static long countSequential(int range) {
+        long count = 0;
+        final int from = 0, to = range;
+        for (int i = from; i < to; i++)
             if (isPrime(i))
-              lc.increment();
-      }));
-    }
-    try {
-      for (Future<?> fut : futures)
-        fut.get();
-    } catch (InterruptedException exn) { 
-      System.out.println("Interrupted: " + exn);
-    } catch (ExecutionException exn) { 
-      throw new RuntimeException(exn.getCause()); 
-    }
-    return lc.get();
-  }
-
-  // General parallel solution, using multiple Callable<Long> tasks
-  private static long countParallelN2(int range, int taskCount) {
-    final int perTask = range / taskCount;
-    List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
-    for (int t=0; t<taskCount; t++) {
-      final int from = perTask * t, 
-        to = (t+1 == taskCount) ? range : perTask * (t+1); 
-      tasks.add(() -> { 
-        long count = 0;  // Task-local counter
-        for (int i=from; i<to; i++)
-          if (isPrime(i))
-            count++;
+                count++;
         return count;
-      });
     }
-    long result = 0;
-    try {
-      List<Future<Long>> futures = executor.invokeAll(tasks);
-      for (Future<Long> fut : futures)
-        result += fut.get();
-    } catch (InterruptedException exn) { 
-      System.out.println("Interrupted: " + exn);
-    } catch (ExecutionException exn) { 
-      throw new RuntimeException(exn.getCause()); 
+
+    // General parallel solution, using multiple (Runnable) tasks
+    private static long countParallelN1(int range, int taskCount) {
+        final int perTask = range / taskCount;
+        final LongAdder lc = new LongAdder();
+        //final LongCounter lc = new LongCounter();
+        List<Future<?>> futures = new ArrayList<Future<?>>();
+        for (int t = 0; t < taskCount; t++) {
+            final int from = perTask * t,
+                    to = (t + 1 == taskCount) ? range : perTask * (t + 1);
+            futures.add(executor.submit(() -> {
+                for (int i = from; i < to; i++)
+                    if (isPrime(i))
+                        lc.increment();
+            }));
+        }
+        try {
+            for (Future<?> fut : futures)
+                fut.get();
+        } catch (InterruptedException exn) {
+            System.out.println("Interrupted: " + exn);
+        } catch (ExecutionException exn) {
+            throw new RuntimeException(exn.getCause());
+        }
+        return lc.longValue();
     }
-    return result;
-  }
 
-  // General parallel solution, using multiple Callable<Void> tasks so
-  // as to be able to submit all to the executor in one method call.
-  private static long countParallelN3(int range, int taskCount) {
-    final int perTask = range / taskCount;
-    final LongCounter lc = new LongCounter();
-    List<Callable<Void>> tasks = new ArrayList<Callable<Void>>();
-    for (int t=0; t<taskCount; t++) {
-      final int from = perTask * t, 
-        to = (t+1 == taskCount) ? range : perTask * (t+1); 
-      tasks.add(() -> { 
-          for (int i=from; i<to; i++)
-            if (isPrime(i))
-              lc.increment();
-          return null;
-      });
+    // General parallel solution, using multiple Callable<Long> tasks
+    private static long countParallelN2(int range, int taskCount) {
+        final int perTask = range / taskCount;
+        List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
+        for (int t = 0; t < taskCount; t++) {
+            final int from = perTask * t,
+                    to = (t + 1 == taskCount) ? range : perTask * (t + 1);
+            tasks.add(() -> {
+                long count = 0;  // Task-local counter
+                for (int i = from; i < to; i++)
+                    if (isPrime(i))
+                        count++;
+                return count;
+            });
+        }
+        long result = 0;
+        try {
+            List<Future<Long>> futures = executor.invokeAll(tasks);
+            for (Future<Long> fut : futures)
+                result += fut.get();
+        } catch (InterruptedException exn) {
+            System.out.println("Interrupted: " + exn);
+        } catch (ExecutionException exn) {
+            throw new RuntimeException(exn.getCause());
+        }
+        return result;
     }
-    try {
-      executor.invokeAll(tasks);
-    } catch (InterruptedException exn) { 
-      System.out.println("Interrupted: " + exn);
-    } 
-    return lc.get();
-  }
 
-  // --- Benchmarking infrastructure ---
+    // General parallel solution, using multiple Callable<Void> tasks so
+    // as to be able to submit all to the executor in one method call.
+    private static long countParallelN3(int range, int taskCount) {
+        final int perTask = range / taskCount;
+        final LongCounter lc = new LongCounter();
+        List<Callable<Void>> tasks = new ArrayList<Callable<Void>>();
+        for (int t = 0; t < taskCount; t++) {
+            final int from = perTask * t,
+                    to = (t + 1 == taskCount) ? range : perTask * (t + 1);
+            tasks.add(() -> {
+                for (int i = from; i < to; i++)
+                    if (isPrime(i))
+                        lc.increment();
+                return null;
+            });
+        }
+        try {
+            executor.invokeAll(tasks);
+        } catch (InterruptedException exn) {
+            System.out.println("Interrupted: " + exn);
+        }
+        return lc.get();
+    }
 
-  // NB: Modified to show microseconds instead of nanoseconds
+    // --- Benchmarking infrastructure ---
 
-  public static double Mark6(String msg, IntToDoubleFunction f) {
-    int n = 10, count = 1, totalCount = 0;
-    double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
-    do { 
-      count *= 2;
-      st = sst = 0.0;
-      for (int j=0; j<n; j++) {
-        Timer t = new Timer();
-        for (int i=0; i<count; i++) 
-          dummy += f.applyAsDouble(i);
-        runningTime = t.check();
-        double time = runningTime * 1e6 / count; // microseconds
-        st += time; 
-        sst += time * time;
-        totalCount += count;
-      }
-      double mean = st/n, sdev = Math.sqrt((sst - mean*mean*n)/(n-1));
-      System.out.printf("%-25s %15.1f us %10.2f %10d%n", msg, mean, sdev, count);
-    } while (runningTime < 0.25 && count < Integer.MAX_VALUE/2);
-    return dummy / totalCount;
-  }
+    // NB: Modified to show microseconds instead of nanoseconds
 
-  public static double Mark7(String msg, IntToDoubleFunction f) {
-    int n = 10, count = 1, totalCount = 0;
-    double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
-    do { 
-      count *= 2;
-      st = sst = 0.0;
-      for (int j=0; j<n; j++) {
-        Timer t = new Timer();
-        for (int i=0; i<count; i++) 
-          dummy += f.applyAsDouble(i);
-        runningTime = t.check();
-        double time = runningTime * 1e6 / count; // microseconds
-        st += time; 
-        sst += time * time;
-        totalCount += count;
-      }
-    } while (runningTime < 0.25 && count < Integer.MAX_VALUE/2);
-    double mean = st/n, sdev = Math.sqrt((sst - mean*mean*n)/(n-1));
-    System.out.printf("%-25s %15.1f us %10.2f %10d%n", msg, mean, sdev, count);
-    return dummy / totalCount;
-  }
+    public static double Mark6(String msg, IntToDoubleFunction f) {
+        int n = 10, count = 1, totalCount = 0;
+        double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
+        do {
+            count *= 2;
+            st = sst = 0.0;
+            for (int j = 0; j < n; j++) {
+                Timer t = new Timer();
+                for (int i = 0; i < count; i++)
+                    dummy += f.applyAsDouble(i);
+                runningTime = t.check();
+                double time = runningTime * 1e6 / count; // microseconds
+                st += time;
+                sst += time * time;
+                totalCount += count;
+            }
+            double mean = st / n, sdev = Math.sqrt((sst - mean * mean * n) / (n - 1));
+            System.out.printf("%-25s %15.1f us %10.2f %10d%n", msg, mean, sdev, count);
+        } while (runningTime < 0.25 && count < Integer.MAX_VALUE / 2);
+        return dummy / totalCount;
+    }
 
-  public static void SystemInfo() {
-    System.out.printf("# OS:   %s; %s; %s%n", 
-                      System.getProperty("os.name"), 
-                      System.getProperty("os.version"), 
-                      System.getProperty("os.arch"));
-    System.out.printf("# JVM:  %s; %s%n", 
-                      System.getProperty("java.vendor"), 
-                      System.getProperty("java.version"));
-    // The processor identifier works only on MS Windows:
-    System.out.printf("# CPU:  %s; %d \"cores\"%n", 
-                      System.getenv("PROCESSOR_IDENTIFIER"),
-                      Runtime.getRuntime().availableProcessors());
-    java.util.Date now = new java.util.Date();
-    System.out.printf("# Date: %s%n", 
-      new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(now));
-  }
+    public static double Mark7(String msg, IntToDoubleFunction f) {
+        int n = 10, count = 1, totalCount = 0;
+        double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
+        do {
+            count *= 2;
+            st = sst = 0.0;
+            for (int j = 0; j < n; j++) {
+                Timer t = new Timer();
+                for (int i = 0; i < count; i++)
+                    dummy += f.applyAsDouble(i);
+                runningTime = t.check();
+                double time = runningTime * 1e6 / count; // microseconds
+                st += time;
+                sst += time * time;
+                totalCount += count;
+            }
+        } while (runningTime < 0.25 && count < Integer.MAX_VALUE / 2);
+        double mean = st / n, sdev = Math.sqrt((sst - mean * mean * n) / (n - 1));
+        System.out.printf("%-25s %15.1f us %10.2f %10d%n", msg, mean, sdev, count);
+        return dummy / totalCount;
+    }
+
+    public static void SystemInfo() {
+        System.out.printf("# OS:   %s; %s; %s%n",
+                System.getProperty("os.name"),
+                System.getProperty("os.version"),
+                System.getProperty("os.arch"));
+        System.out.printf("# JVM:  %s; %s%n",
+                System.getProperty("java.vendor"),
+                System.getProperty("java.version"));
+        // The processor identifier works only on MS Windows:
+        System.out.printf("# CPU:  %s; %d \"cores\"%n",
+                System.getenv("PROCESSOR_IDENTIFIER"),
+                Runtime.getRuntime().availableProcessors());
+        java.util.Date now = new java.util.Date();
+        System.out.printf("# Date: %s%n",
+                new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(now));
+    }
 }
 
 class LongCounter {
-  private long count = 0;
-  public synchronized void increment() {
-    count = count + 1;
-  }
-  public synchronized long get() { 
-    return count; 
-  }
+    private long count = 0;
+
+    public synchronized void increment() {
+        count = count + 1;
+    }
+
+    public synchronized long get() {
+        return count;
+    }
 }
--- exercise05/TestDownload.java
+++ exercise05/TestDownload.java
@@ -2,42 +2,117 @@
 // sestoft@itu.dk * 2014-09-19
 
 import java.net.URL;
+import java.util.HashMap;
+import com.sun.javafx.collections.MappingChange.Map;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.function.IntToDoubleFunction;
 
 public class TestDownload {
 
-  private static final String[] urls = 
-  { "http://www.itu.dk", "http://www.di.ku.dk", "http://www.miele.de",
-    "http://www.microsoft.com", "http://www.amazon.com", "http://www.dr.dk",
-    "http://www.vg.no", "http://www.tv2.dk", "http://www.google.com",
-    "http://www.ing.dk", "http://www.dtu.dk", "http://www.eb.dk", 
-    "http://www.nytimes.com", "http://www.guardian.co.uk", "http://www.lemonde.fr",   
-    "http://www.welt.de", "http://www.dn.se", "http://www.heise.de", "http://www.wsj.com", 
-    "http://www.bbc.co.uk", "http://www.dsb.dk", "http://www.bmw.com", "https://www.cia.gov" 
-  };
+    private static final ExecutorService executor = Executors.newWorkStealingPool();
+    private static final String[] urls =
+            {"http://www.itu.dk", "http://www.di.ku.dk", "http://www.miele.de",
+                    "http://www.microsoft.com", "http://www.amazon.com", "http://www.dr.dk",
+                    "http://www.vg.no", "http://www.tv2.dk", "http://www.google.com",
+                    "http://www.ing.dk", "http://www.dtu.dk", "http://www.eb.dk",
+                    "http://www.nytimes.com", "http://www.guardian.co.uk", "http://www.lemonde.fr",
+                    "http://www.welt.de", "http://www.dn.se", "http://www.heise.de", "http://www.wsj.com",
+                    "http://www.bbc.co.uk", "http://www.dsb.dk", "http://www.bmw.com", "https://www.cia.gov"
+            };
 
-  public static void main(String[] args) throws IOException {
-    String url = "https://www.wikipedia.org/";
-    String page = getPage(url, 10);
-    System.out.printf("%-30s%n%s%n", url, page);
-  }
+    public static void main(String[] args) throws IOException {
+        // String url = "https://www.wikipedia.org/";
+        // String page = getPage(url, 10);
 
-  public static String getPage(String url, int maxLines) throws IOException {
-    // This will close the streams after use (JLS 8 para 14.20.3):
-    try (BufferedReader in 
-         = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
-      StringBuilder sb = new StringBuilder();
-      for (int i=0; i<maxLines; i++) {
-        String inputLine = in.readLine();
-        if (inputLine == null)
-          break;
-        else
-          sb.append(inputLine).append("\n");
-      }
-      return sb.toString();
+        HashMap<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < 5; i++) {
+            Timer timer = new Timer();
+            map = getPagesParallel(urls, 200);
+            double time = timer.check() * 1e9;
+            System.out.printf("%6.1f ns%n", time);
+        }
+        for (HashMap.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println("Nr of charecters in body: " + entry.getValue().length());
+        }
     }
-  }
-}
 
+
+    public static HashMap<String, String> getPagesParallel(String[] urls, int maxLines) {
+        HashMap<String, String> urlMap = new HashMap<String, String>();
+        List<Future<?>> futures = new ArrayList<Future<?>>();
+        for (int x = 0; x < urls.length; x++) {
+            int x_temp = x;
+            futures.add(executor.submit(() ->
+            {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(new URL(urls[x_temp]).openStream()))) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < maxLines; i++) {
+                        String inputLine = in.readLine();
+                        if (inputLine == null)
+                            break;
+                        else
+                            sb.append(inputLine).append("\n");
+                    }
+                    urlMap.put(urls[x_temp], sb.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }));
+        }
+        try {
+            for (Future<?> fut : futures)
+                fut.get();
+        } catch (InterruptedException exn) {
+            System.out.println("Interrupted: " + exn);
+        } catch (ExecutionException exn) {
+            throw new RuntimeException(exn.getCause());
+        }
+        return urlMap;
+    }
+
+
+    public static String getPage(String url, int maxLines) throws IOException {
+        // This will close the streams after use (JLS 8 para 14.20.3):
+        try (BufferedReader in
+                     = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < maxLines; i++) {
+                String inputLine = in.readLine();
+                if (inputLine == null)
+                    break;
+                else
+                    sb.append(inputLine).append("\n");
+            }
+            return sb.toString();
+        }
+    }
+
+    public static HashMap<String, String> getPages(String[] urls, int maxLines) throws IOException {
+        HashMap<String, String> urlMap = new HashMap<String, String>();
+        for (int x = 0; x < urls.length; x++) {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(new URL(urls[x]).openStream()))) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < maxLines; i++) {
+                    String inputLine = in.readLine();
+                    if (inputLine == null)
+                        break;
+                    else
+                        sb.append(inputLine).append("\n");
+                }
+                urlMap.put(urls[x], sb.toString());
+            }
+        }
+        return urlMap;
+    }
+}
