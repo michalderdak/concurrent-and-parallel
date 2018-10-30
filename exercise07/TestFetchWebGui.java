edd9e69ba568b8809863121ec7ffa6c98f891359
Michal Derdak <michal.derdak@Michals-MacBook-Pro.local>
2018-10-30 23:06:21 +0100

    part one of exercise 07

--- exercise07/TestFetchWebGui.java
+++ exercise07/TestFetchWebGui.java
@@ -9,9 +9,10 @@ import javax.swing.*;
 
 // For the SwingWorker subclass
 import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
-
+import java.util.concurrent.atomic.AtomicInteger;
 // For downloading web pages
 import java.net.URL;
 import java.io.BufferedReader;
@@ -23,6 +24,19 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class TestFetchWebGui {
+  
+  private static final String[] urls = 
+  { "http://www.itu.dk", "http://www.di.ku.dk", "http://www.miele.de",
+    "http://www.microsoft.com", "http://www.dr.dk",
+    "http://www.vg.no", "http://www.tv2.dk", "http://www.google.com",
+    "http://www.ing.dk", "http://www.dtu.dk", "http://www.eb.dk", 
+    "http://www.nytimes.com", "http://www.guardian.co.uk", "http://www.lemonde.fr",   
+    "http://www.welt.de", "http://www.dn.se", "http://www.heise.de", "http://www.wsj.com", 
+    "http://www.bbc.co.uk", "http://www.dsb.dk", "http://www.bmw.com", "https://www.cia.gov" 
+  };
+
+  private static AtomicInteger atomicProgress = new AtomicInteger();
+
   public static void main(String[] args) {
       //badFetch();
     goodFetch();
@@ -63,9 +77,9 @@ public class TestFetchWebGui {
 
   // (1) Use a SwingWorker subclass to perform the work in the
   // doInBackground method on a different thread, cause the event
-  // thread to show the final result using the done method.  (2) Add a
+  // thread to show the final result using the done method.  (3) Add a
   // progress bar that displays the fraction of web pages fetched so
-  // far.  (3) Add the possibility of cancellation by testing
+  // far.  (2) Add the possibility of cancellation by testing
   // isCancelled() in the doInBackground method, and catch the
   // CancellationException in the done method.  (4) Display the
   // results as they become available by letting doInBackground call
@@ -76,6 +90,7 @@ public class TestFetchWebGui {
   // textArea.
 
   private static void goodFetch() {
+    // Creation of GUI
     final JFrame frame = new JFrame("TestFetchWebGui");
     final JPanel outerPanel = new JPanel(), 
       buttonPanel = new JPanel();
@@ -90,39 +105,93 @@ public class TestFetchWebGui {
     final TextArea textArea = new TextArea(25, 80);
     textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
     outerPanel.add(textArea, BorderLayout.WEST);
+
+    // Create new worker for each url
+    List<DownloadWorker> downloadWorkers = new ArrayList<DownloadWorker>();
+    for (String url : urls) {
+      downloadWorkers.add(new DownloadWorker(url, textArea));
+    }
+
+    // Set button listneners for each worker
     // (1) Use a background thread, not the event thread, for work
-    DownloadWorker downloadTask = new DownloadWorker(textArea);
-    fetchButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          downloadTask.execute();
-        }});
-    // (2) Add a progress bar
+    // +
+    // (2) Enable cancellation
+    // +
+    // (3) Add a progress bar
+
     JProgressBar progressBar = new JProgressBar(0, 100);
-    //    progressBar.setValue(50);
     outerPanel.add(progressBar, BorderLayout.SOUTH);
-    downloadTask.addPropertyChangeListener(new PropertyChangeListener() {
+
+    for (DownloadWorker worker : downloadWorkers) {
+      // Single workers for each url
+      fetchButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          worker.execute();
+        }});
+
+      // cancel
+      cancelButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          worker.cancel(false);
+      }});
+
+      // Progress
+      worker.addPropertyChangeListener(new PropertyChangeListener() {
         public void propertyChange(PropertyChangeEvent e) {
           if ("progress".equals(e.getPropertyName())) {
             progressBar.setValue((Integer)e.getNewValue());
-          }}});
-    // (3) Enable cancellation
-    cancelButton.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          downloadTask.cancel(false);
-        }});
+          }
+      }});
+    }
+    
     frame.pack(); frame.setVisible(true);
   }
 
-  private static final String[] urls = 
-  { "http://www.itu.dk", "http://www.di.ku.dk", "http://www.miele.de",
-    "http://www.microsoft.com", "http://www.dr.dk",
-    "http://www.vg.no", "http://www.tv2.dk", "http://www.google.com",
-    "http://www.ing.dk", "http://www.dtu.dk", "http://www.eb.dk", 
-    "http://www.nytimes.com", "http://www.guardian.co.uk", "http://www.lemonde.fr",   
-    "http://www.welt.de", "http://www.dn.se", "http://www.heise.de", "http://www.wsj.com", 
-    "http://www.bbc.co.uk", "http://www.dsb.dk", "http://www.bmw.com", "https://www.cia.gov" 
-  };
+  private static class DownloadWorker extends SwingWorker<String,String> {
+    private final TextArea textArea; 
+    private final String _url;
 
+    public DownloadWorker(String url, TextArea textArea) {
+      this.textArea = textArea;
+      _url = url;
+    }
+
+    // Do work in background
+    public String doInBackground() {
+      System.out.println("Fetching " + _url);
+      
+      String page = getPage(_url, 200);
+      String result = String.format("%-40s%7d%n", _url, page.length());
+
+      if (isCancelled())
+        throw new CancellationException();
+
+      setProgress((100 * atomicProgress.incrementAndGet()) / urls.length); // (3)
+
+      return result;
+    }
+  
+    // Is called when publish() is used in doInBackground
+    public void process(List<String> result) {
+    }
+
+    // Called in the event thread when done() has terminated, whether
+    // by completing or by being cancelled.
+    public void done() {
+      try { 
+        textArea.append(get());
+      } 
+      catch (InterruptedException e) {
+      }
+      catch (ExecutionException e) { 
+      }
+      catch (CancellationException e) { 
+        textArea.append("Cancelled"); // (2)
+      } 
+    }
+  }
+
+  // Helper function to download the pages
   public static String getPage(String url, int maxLines) {
     try {
       // This will close the streams after use (JLS 8 para 14.20.3):
@@ -136,56 +205,11 @@ public class TestFetchWebGui {
           else
             sb.append(inputLine).append("\n");
         }
-	return sb.toString();
+	      return sb.toString();
       }
     } catch (IOException exn) { 
       System.out.println(exn); 
       return "";
-    }
-  }
-
-  static class DownloadWorker extends SwingWorker<String,String> {
-    private final TextArea textArea; 
-
-    public DownloadWorker(TextArea textArea) {
-      this.textArea = textArea;
-    }
-
-    // Called on a separate worker thread, not the event thread, and
-    // hence cannot write to textArea.
-    public String doInBackground() {
-      StringBuilder sb = new StringBuilder();
-      int count = 0;
-      for (String url : urls) {
-        if (isCancelled())			    // (3)
-           break;
-	System.out.println("Fetching " + url);
-        String page = getPage(url, 200),
-          result = String.format("%-40s%7d%n", url, page.length());
-        sb.append(result); // (1)
-        setProgress((100 * ++count) / urls.length); // (2)
-        publish(result); // (4)
-      }
-      return sb.toString();
-    }
-  
-    // (4) Called on the event thread to process results previously
-    // published by calls to the publish method.
-    public void process(List<String> results) {
-        System.err.println("run process");
-        for (String result : results)
-        textArea.append(result);
-    }
-
-    // Called in the event thread when done() has terminated, whether
-    // by completing or by being cancelled.
-    public void done() {
-      try { 
-        textArea.append(get());
-        textArea.append("Done"); 
-      } catch (InterruptedException exn) { }
-        catch (ExecutionException exn) { throw new RuntimeException(exn.getCause()); }
-        catch (CancellationException exn) { textArea.append("Cancelled"); }  // (3)
     }
   }
 }
